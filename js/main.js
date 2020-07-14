5ccbc431aa93d33c0d3e479b69290d307a6eff6e
slowkkuruma <stevan.han82@gmail.com>
2020-07-14 13:33:34 -0700

    commit

--- js/main.js
+++ js/main.js
@@ -47,52 +47,7 @@ function shuffle(deck) {
 //Hit function
 
 //Stay function
-console.log("It Works");
 
 
 
 
-//Define variables
-
-const player = "You";
-const dealer = "Dealer";
-
-let playerHand = [];
-let dealerHand = [];
-
-let pScore = 0;
-let dScore = 0;
-
-
-//Define DOM elements
-
-//Game
-
-//Shuffle function
-
-//Fisher-Yate's algorithm
-
-function shuffle(deck) {
-    let i = 0,
-        j = 0,
-        temp = null;
-    for (i = deck.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-}
-
-
-
-//Deal function
-
-//checkScore function
-
-//Update scores
-
-
-//Hit function
-
-//Stay function
